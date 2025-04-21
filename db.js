672a62d2f3f2fb0a3464a64d79934fc937938186
Quinton30 <122327951+Quinton30@users.noreply.github.com>
2025-04-21 13:12:28 +0300

    Update db.js

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ require('dotenv').config(); // Load environment variables
 
 const { Pool } = require('pg');
 
+//credentials for the database on render
 const pool = new Pool({
     user: 'students_fqe5_user',
     host: 'cvi01adrie7s73ebi45g-a.oregon-postgres.render.com',
