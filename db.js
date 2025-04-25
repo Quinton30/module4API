0575632668ac42eaff072e24c92177083d8b386c
Quinton30 <122327951+Quinton30@users.noreply.github.com>
2025-04-25 17:31:20 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ require('dotenv').config(); // Load environment variables
 const { Pool } = require('pg');
 
 //credentials for the database on render
-const pool = new Pool({
+/*const pool = new Pool({
     user: 'students_fqe5_user',
     host: 'cvi01adrie7s73ebi45g-a.oregon-postgres.render.com',
     database: 'students_fqe5',
@@ -12,6 +12,17 @@ const pool = new Pool({
     ssl: {
         rejectUnauthorized: false,
     }
-});
+}); Database expired */
+
+const pool = new Pool({
+  user: 'postgres.auqsxkehosbvhviedypm',
+  host: 'aws-0-us-east-2.pooler.supabase.com',
+  database: 'postgres',
+  password: '#5@w$MM5SdSG8CK',
+  port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+})
 
 module.exports = pool;
